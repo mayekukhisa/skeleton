0da18b3cf6ec1f29fab2fec00a29e809e5cc15f8
Mayeku Khisa <101919651+mayekukhisa@users.noreply.github.com>
2023-01-01 10:46:14 +0300

    initial commit

---
+++ README.md
@@ -0,0 +1,56 @@
+# Skeleton
+
+Skeleton is a template generator tool for helping developers quickly set up new projects.
+
+## Features
+
+> **Note:** There are currently no features yet. This tool is in its early stages of development.
+
+## Getting Started
+
+This part shows how to obtain a copy of the project and get the tool running on your local machine.
+
+### System requirement
+
+This project depends on the tool below being available in your environment:
+
+-  JDK 11+
+
+### Downloading the codebase
+
+[Fork this repo][1] on GitHub and clone your fork. Alternatively, you can [download this repo][2] as a zip and extract it.
+
+### Running the tool
+
+> **Note:** Use PowerShell or Git Bash to run commands on Windows.
+
+1. Navigate into the project's root directory.
+
+2. Install the project as a distribution:
+
+   ```shell
+   ./gradlew installDist
+   ```
+
+3. Run the tool from the build directory:
+
+   ```shell
+   ./build/install/skeleton/bin/skeleton --help
+   ```
+
+Alternatively, you can open and run the project in [IntelliJ IDEA][3]. For more on how to run the tool with arguments, see [here][4].
+
+## Author
+
+Mayeku Khisa - _Maintainer_ - [@mayekukhisa][5].
+
+## License
+
+This tool is available under the [Apache-2.0 License][6].<br/>Copyright &copy; 2023 Mayeku Khisa
+
+[1]: https://github.com/mayekukhisa/skeleton/fork
+[2]: https://github.com/mayekukhisa/skeleton/archive/refs/heads/main.zip
+[3]: https://www.jetbrains.com/idea/
+[4]: https://www.jetbrains.com/help/idea/running-applications.html
+[5]: https://github.com/mayekukhisa
+[6]: LICENSE
---
+++ build.gradle.kts
@@ -0,0 +1,97 @@
+/*
+ * Copyright (c) 2023 Mayeku Khisa
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
+
+plugins {
+   application
+   id("com.diffplug.spotless") version "6.12.0"
+   kotlin("jvm") version "1.7.21"
+}
+
+group = "io.github.mayekukhisa.skeleton"
+version = "0.1.0"
+
+repositories {
+   mavenCentral()
+}
+
+spotless {
+   val indentSpaces = 3
+   val maxLineLength = 120
+
+   /*
+    Spotless does not yet support .editorconfig settings with Ktlint.
+    We have to provide them manually.
+    */
+   mapOf(
+      "indent_size" to indentSpaces,
+      "max_line_length" to maxLineLength,
+      "ij_kotlin_allow_trailing_comma" to true,
+      "ij_kotlin_allow_trailing_comma_on_call_site" to true,
+   ).let {
+      val licenseHeaderPath = "spotless/copyright.kt"
+
+      kotlin {
+         target("**/*.kt")
+         targetExclude("**/build/**/*.kt", licenseHeaderPath)
+         ktlint().editorConfigOverride(it)
+         licenseHeaderFile(rootProject.file(licenseHeaderPath))
+            .updateYearWithLatest(true)
+      }
+
+      kotlinGradle {
+         target("**/*.kts")
+         targetExclude("**/build/**/*.kts")
+         ktlint().editorConfigOverride(it)
+         licenseHeaderFile(rootProject.file(licenseHeaderPath), "^(?![\\/ ]\\*).*")
+            .updateYearWithLatest(true)
+      }
+   }
+
+   mapOf(
+      "printWidth" to maxLineLength,
+      "tabWidth" to indentSpaces,
+   ).let {
+      json {
+         target("**/*.json")
+         targetExclude("**/build/**/*.json")
+         prettier().config(it)
+      }
+
+      format("Markdown") {
+         target("**/*.md")
+         targetExclude("**/build/**/*.md")
+         prettier().config(it)
+      }
+   }
+}
+
+dependencies {
+   implementation("com.github.ajalt.clikt:clikt:3.5.1")
+   testImplementation(kotlin("test"))
+}
+
+tasks.test {
+   useJUnitPlatform()
+}
+
+tasks.withType<KotlinCompile> {
+   kotlinOptions.jvmTarget = "11"
+}
+
+application {
+   mainClass.set("$group.MainKt")
+}
